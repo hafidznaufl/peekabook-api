856312c7db60f45cae8dea1a073bf77da6258bd5
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-10-08 18:54:02 +0700

    feat(middleware): implementing middleware

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"rentabook/routes"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -13,6 +14,14 @@ func main() {
 	DB := config.ConnectDB()
 
 	routes.Init(app, DB)
+
+	app.Pre(middleware.RemoveTrailingSlash())
+	app.Use(middleware.CORS())
+	app.Use(middleware.LoggerWithConfig(
+		middleware.LoggerConfig{
+			Format: "method=${method}, uri=${uri}, status=${status}, time=${time_rfc3339}\n",
+		},
+	))
 
 	app.Logger.Fatal(app.Start(":8000"))
 
